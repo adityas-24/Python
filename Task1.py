d79c81030a4c2ba2b81c207e34053ae661a544aa
adityas-24 <adityasatav06@gmail.com>
2025-08-20 15:39:24 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,14 @@
+a=(int(input("Enter First number:")))
+b=(int(input("Enter Second number:")))
+addition=a+b
+print("Addition=",addition)
+substraction=a-b
+print("Substraction=",substraction)
+multiply=a*b
+print("Multiplication=",multiply)
+if b !=0:
+    division=a/b
+else:
+    division="Invalid input"
+
+print("Division=",division)
---
+++ Task2.py
@@ -0,0 +1,3 @@
+a=(str(input("Enter your first name:")))
+b=(str(input("Enter your last name:")))
+print(f"Hello,{a} {b} Welcome to the python program")
